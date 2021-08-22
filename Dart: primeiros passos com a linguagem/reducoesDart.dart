b6bacac537be4288d1f41f46f2485fbbce35b19f
Marianna <45279003+mariannabourges@users.noreply.github.com>
2021-08-22 02:06:31 -0300

    Create reducoesDart.dart

---
+++ Dart: primeiros passos com a linguagem/reducoesDart.dart
@@ -0,0 +1,6 @@
+///variavel++ substitui variavel = variavel + 1
+///variavel-- substitui variavel = variavel - 1
+///variavel += outraVariavel substitui variavel = variavel + outraVariavel
+///variavel -= outraVariavel substitui variavel = variavel - outraVariavel
+///variavel *= outraVariavel substitui variavel = variavel * outraVariavel
+///variavel /= outraVariavel substitui variavel = variavel / outraVariavel
